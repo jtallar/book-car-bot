065f5e76ae7ccc59f1ff89eb0ad8c2339da99a2a
Julian Tallar <jtallar@itba.edu.ar>
2021-10-24 17:51:26 -0300

    fixed telebot import

--- app.py
+++ app.py
@@ -5,7 +5,8 @@
 import re
 from flask import Flask, request
 import telegram
-from telebot.credentials import bot_token, bot_user_name,URL
+import telebot.credentials as credentials
+from credentials import bot_token, bot_user_name,URL
 
 global bot
 global TOKEN
